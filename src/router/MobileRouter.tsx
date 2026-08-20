a5350e41c1bb370e33c0569272143b18584b9777
dongzc-1958825 <dongzc@263.net>
2026-08-20 10:48:12 +0800

    fix: 修正重置密码路由路径

--- src/router/MobileRouter.tsx
+++ src/router/MobileRouter.tsx
@@ -7,16 +7,18 @@ import MobileCaseDetailPage from '../pages/mobile/MobileCaseDetailPage';
 import MobileFavoritesPage from '../pages/mobile/MobileFavoritesPage';
 import MobileProfilePage from '../pages/mobile/MobileProfilePage';
 import CreateCasePage from '../pages/CreateCasePage/index';
+import ResetPasswordPage from '../pages/ResetPasswordPage';
 
 const MobileRouter: React.FC = () => {
   return (
     <Routes>
-      <Route path="/mobile" element={<MobileHomePage />} />
-      <Route path="/mobile/cases" element={<MobileCasesPage />} />
-      <Route path="/mobile/cases/:id" element={<MobileCaseDetailPage />} />
-      <Route path="/mobile/cases/create" element={<CreateCasePage />} />
-      <Route path="/mobile/favorites" element={<MobileFavoritesPage />} />
-      <Route path="/mobile/profile" element={<MobileProfilePage />} />
+      <Route path="mobile" element={<MobileHomePage />} />
+      <Route path="mobile/cases" element={<MobileCasesPage />} />
+      <Route path="mobile/cases/:id" element={<MobileCaseDetailPage />} />
+      <Route path="mobile/cases/create" element={<CreateCasePage />} />
+      <Route path="mobile/favorites" element={<MobileFavoritesPage />} />
+      <Route path="mobile/profile" element={<MobileProfilePage />} />
+      <Route path="reset-password" element={<ResetPasswordPage />} />
     </Routes>
   );
 };
